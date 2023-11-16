500fd4010d94744166e057487b16079346a927e9
Brian <bstanton773@gmail.com>
2023-11-16 10:52:47 -0600

    Add an answer form that will say correct or not

--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
 
 type QuestionType = {
@@ -18,15 +19,42 @@ type Props = {
 
 
 export default function QuestionCard({ question }: Props) {
-    const [showAnswer, setShowAnswer] = useState(false);
+    // const [showAnswer, setShowAnswer] = useState(false);
+    const [guess, setGuess] = useState('');
+    const [correct, setCorrect] = useState(false);
+    const [formSubmitted, setFormSubmitted] = useState(false)
+
+    const handleInputChange = (e:React.ChangeEvent<HTMLInputElement>) => {
+        setGuess(e.target.value);
+    }
+
+    const handleFormSubmit = (e:React.FormEvent) => {
+        e.preventDefault();
+        setFormSubmitted(true);
+        if (guess.toLowerCase() === question.answer.toLowerCase()){
+            setCorrect(true);
+        } else {
+            setCorrect(false);
+        }
+    }
+
     return (
         <Card className='my-3'>
             <Card.Body>
                 <Card.Title>{question.question}</Card.Title>
-                {showAnswer && <Card.Text>{question.answer}</Card.Text>}
+                {/* {showAnswer && <Card.Text>{question.answer}</Card.Text>}
                 <Button variant={showAnswer ? 'danger' :'success'} onClick={() => setShowAnswer(!showAnswer)}>
                     {showAnswer ? 'Hide Answer'  : 'Reveal Answer'}
-                </Button>
+                </Button> */}
+                <Form onSubmit={handleFormSubmit}>
+                    <Form.Control name='guess' placeholder='Enter Your Guess' value={guess} onChange={handleInputChange} />
+                    <Button type='submit'>Check Guess</Button>
+                </Form>
+                { formSubmitted ? correct ? (
+                    <h4>Congratulations you have the correct answer!</h4>
+                ) : (
+                    <h4>You are incorrect. The correct answer is {question.answer}</h4>
+                ) : null}
             </Card.Body>
         </Card>
     )
